5b7009c49bf31c9994d9c28e367db300e624ce15
artviet0511 <vietart@gmail.com>
2020-06-21 21:55:17 +0700

    DT

--- detai/detai_1/app.js
+++ detai/detai_1/app.js
@@ -1,78 +1,94 @@
-// nut gio hang
-let itemgiohang = document.getElementById("itemgiohang");
-let giohang = document.getElementById("giohang");
-itemgiohang.addEventListener("click", function () {
-    if (giohang.style.display == "none") {
-        giohang.style.display = "block";
-    } else {
-        giohang.style.display = "none";
+let cart = [];
+function showData() {
+    let mainMenu = document.getElementById("main-menu");
+    for (let category of fake_data) {
+        let html = `
+        <div class="out">
+            <h1 id="noibat" class="item">${category.title}</h1>
+            <div class="flex">`;
+
+        for (let menuItem of category.data) {
+            html += `        
+                <div class="menu-item">
+                    <img class="image" src="${menuItem.image}">
+                    <div>
+                        <h1 class="name">${menuItem.name}</h1>
+                        <h1 class="price">${menuItem.price}</h1>
+                        <input type="number" class="number" min="1" max="${menuItem.number}" value="1">
+                        <h1 class="buy-now" onclick="addToCart(event)">MUA NGAY</h1>
+                    </div>
+                </div>`;
+        }
+
+        html += `
+                </div>
+            </div>
+        </div>`;
+        mainMenu.innerHTML += html;
     }
-});
-// let x = document.getElementById("x")
-// x.addEventListener('click', function () {
-//     giohang.style.display = "none";
-// });
-
-// cap nhat gia
-function updatebuy() {
-    let buy_item = document.getElementsByClassName("main-buy")[0];
-    let fick_buys = buy_item.getElementsByClassName("fick-buy");
-    let loop = 0;
-    for (let i = 0; i < fick_buys.length; i++) {
-        fick_buy = fick_buys[i];
-        let price_item = parseFloat(fick_buy.getElementsByClassName("gia-buy")[0].innerText);
-        let soluong = fick_buy.getElementsByClassName("soluong-buy")[0].value;
-        fick_buy.getElementsByClassName("tong-buy")[0].innerText = (price_item * soluong) + "VND"
-        loop = loop + (price_item * soluong);
-    };
-    document.getElementsByClassName('thanh tien')[0].innerText = loop + "VND";
 }
 
-// them vao gio
-let add_mua = document.getElementsByClassName("datmua");
-for (let i = 0; i < add_mua.length; i++) {
-    add = add_mua[i];
-    add.addEventListener("click", function (event) {
-        let button = event.target;
-        let product = button.parentElement.parentElement;
-        let title = product.getElementsByClassName("name")[0].innerText;
-        let price = product.getElementsByClassName("gia")[0].innerText;
-        additemtobuy(title, price);
-        updatebuy();
-    });
-};
-function additemtobuy(title, price) {
-    let itembuy = document.createElement("div");
-    itembuy.classList.add('fick-buy');
-    let itemmainbuy = document.getElementsByClassName('main-buy')[0];
-    let name_buy = document.getElementsByClassName("name-buy")
-    for (let i = 0; i < name_buy.length; i++) {
-        if (name_buy[i].innerText == title) {
-            alert('san pham da them vao gio hang');
-            return;
-        };
+function addToCart(event) {
+    let info = event.target.parentNode;
+    let item = {
+        name: info.querySelector(".name").innerText,
+        price: info.querySelector(".price").innerText,
+        number: info.querySelector(".number").value
     };
-    let itembuycontent = `
-    <div class="name-buy">${title}</div>
-    <div class="gia-buy">${price}</div>
-    <input class="soluong-buy" type="number" value="1">
-    <div class="tong-buy">${parseFloat(price)}</div>
-    <button class="xoa" type="button">Xóa</button>
-    `
-    itembuy.innerHTML = itembuycontent;
-    itemmainbuy.appendChild(itembuy);
+    cart.push(item);
 
-    // xoa 1 muc trong gio hang
-    itembuy.getElementsByClassName("xoa")[0].addEventListener('click', function (event) {
-        event.target.parentElement.remove();
-        updatebuy()
-    });
-
-    //thay doi so luong
-    itembuy.getElementsByClassName("soluong-buy")[0].addEventListener('change', function () {
-        if (isNaN(event.target.value) || event.target.value <= 0) {
-            event.target.value = 1;
-        };
-        updatebuy();
-    })
 }
+showData();
+
+// add to Localstage
+function showLocal() {
+    localStorage.setItem("key_data", JSON.stringify(fake_data));
+    let jsondata = localStorage.getItem("key_data");
+    let data_out = JSON.parse(jsondata);
+    console.log(data_out);
+}
+showLocal();
+
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close");
+
+for (i = 0; i < span.length; i++) {
+    click = span[i];
+    click.onclick = function () {
+        modal.style.display = "none";
+    };
+}
+
+
+btn.addEventListener("click", function () {
+    modal.style.display = "block";
+}) 
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function showDataCart() {
+    console.log(cart)
+    let itemCart = document.querySelector(".item-cart");
+    let html = " " 
+    for (let key of cart) {
+        html += `<div class="item">
+        <div class="item-name">${key.name}</div>
+        <div class="item-number">${key.number}</div>
+        <div class="item-price">${key.price}</div>
+    </div>`
+    }
+    let sum = Number(document.getElementById("price-number").innerText);
+    console.log(sum);
+    
+    console.log(html);
+    itemCart.innerHTML = html ;
+}
+
+
